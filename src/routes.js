31a1444e940af60000cc5258657b914dfb39b8d2
gilbertohnrq <gilbertohnrq@gmail.com>
2019-11-16 20:23:43 -0300

    Creating user model

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,15 @@
-const { Router } = require('express');
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernantes',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '1238712387',
+  });
+  return res.json(user);
+});
 
 export default routes;
